6bb7905089ed172cfaf54723e11450ed30276df4
anangben <anangben932@gmail.com>
2026-04-19 19:54:17 +0000

    updated AuctionPage.jsx

--- src/components/AuctionSection.jsx
+++ src/components/AuctionSection.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import { useAuth } from "../../context/AuthContext"; 
+import { useAuth } from "../context/AuthContext"; 
 import banana from "../assets/images/banana.jpg";
 import tomatoes from "../assets/images/tomatoes.png";
 import lettuce from "../assets/images/lettuce.png";
